ba13b3875f1767c4230c1d3de6472ea12b2baa55
AngomRanjan <angomranjan@gmail.com>
2022-08-02 19:50:44 +0530

    Implement Click Event Handler

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState((state) => calculate(state, e.target.innerText));
   }
 
   render() {
@@ -104,15 +113,16 @@ class Calculator extends Component {
         type: 'ops',
       },
     ];
+    const { total, next } = this.state;
     return (
       <>
         <section className="container">
-          <div className="display">0</div>
+          <div className="display">{next || total || 0}</div>
           <section className="grid-container">
             {items.map((item) => (
-              <div key={item.id} className={`grid-item ${item.type}`} id={`item${item.id}`}>
+              <button type="button" key={item.id} className={`grid-item ${item.type}`} id={`item${item.id}`} onClick={this.handleClick}>
                 {item.text}
-              </div>
+              </button>
             ))}
           </section>
         </section>
